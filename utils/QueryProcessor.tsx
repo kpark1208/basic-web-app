99d8f0628884e0d93ff985255326dabc960cf8e0
Hyeonjae Park <hyeonjaep@Hyeonjaes-MacBook-Pro.local>
2025-10-07 10:02:17 -0400

    query 1

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "27062dd0";
   }
 
   if (query.toLowerCase().includes("id")) {
